7b07c0d547a524da906d0042e1f052bf20282af0
Elena Kolesnikova <koleslena@yandex.ru>
2019-05-16 15:17:18 +0600

    fixed zoom for img pages, fixed thumbnails click

--- apps/viewer/src/app/thumbnails/thumbnails.component.ts
+++ apps/viewer/src/app/thumbnails/thumbnails.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit, SimpleChanges} from '@angular/core';
-import {PageModel} from "@groupdocs-total-angular/common-components";
+import {NavigateService, PageModel} from "@groupdocs-total-angular/common-components";
 
 @Component({
   selector: 'gd-thumbnails',
@@ -11,7 +11,7 @@ export class ThumbnailsComponent implements OnInit {
   @Input() pages: PageModel[];
   @Input() isHtmlMode: boolean;
 
-  constructor() {
+  constructor(private _navigateService: NavigateService) {
   }
 
   ngOnInit() {
@@ -25,4 +25,7 @@ export class ThumbnailsComponent implements OnInit {
     return dataImagePngBase64;
   }
 
+  openPage(pageNumber: number) {
+    this._navigateService.navigateTo(pageNumber);
+  }
 }
