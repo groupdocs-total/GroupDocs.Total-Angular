25b8074b19b22c38a5bb0436cb4484f43f33a388
Pavel Egorov <egorovpasha@gmail.com>
2019-05-30 10:14:44 +0400

    Release correction (#11)

--- apps/comparison-e2e/src/integration/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { getGreeting } from '../support/app.po';
-
-describe('Hello Nx', () => {
-  beforeEach(() => cy.visit('/comparison'));
-
-  it('should display welcome message', () => {
-    
-  });
-});
--- apps/total/src/app/app-routing.module.ts
+++ apps/total/src/app/app-routing.module.ts
@@ -2,25 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {ViewerModule} from "../../../viewer/src/app/viewer.module";
-import {SignatureModule} from "../../../signature/src/app/signature.module";
-import {AnnotationModule} from "../../../annotation/src/app/annotation.module";
-import {ComparisonModule} from "../../../comparison/src/app/comparison.module";
 import {ViewerAppComponent} from "../../../viewer/src/app/viewer-app.component";
-import {SignatureAppComponent} from "../../../signature/src/app/signature-app.component";
-import {AnnotationAppComponent} from "../../../annotation/src/app/annotation-app.component";
-import {ComparisonAppComponent} from "../../../comparison/src/app/comparison-app.component";
 import {TotalViewComponent} from "./total-view/total-view.component";
 
 const routes: Routes = [
   {path: '', component: TotalViewComponent},
   {path: 'viewer',
     component: ViewerAppComponent},
-  {path: 'signature',
-    component: SignatureAppComponent},
-  {path: 'annotation',
-    component: AnnotationAppComponent},
-  {path: 'comparison',
-    component: ComparisonAppComponent},
 ];
 
 @NgModule({
@@ -28,9 +16,6 @@ const routes: Routes = [
   imports: [
     CommonModule,
     ViewerModule,
-    SignatureModule,
-    AnnotationModule,
-    ComparisonModule,
     RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
