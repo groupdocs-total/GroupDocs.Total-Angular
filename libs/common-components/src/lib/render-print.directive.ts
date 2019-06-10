f437501236698786efbc445e671e6701192ba9ae
pavelteplitsky <pavel.teplitsky@aspose.com>
2019-06-10 09:50:52 +0300

    Fixed image print

--- libs/common-components/src/lib/render-print.directive.ts
+++ libs/common-components/src/lib/render-print.directive.ts
@@ -21,35 +21,40 @@ export class RenderPrintDirective {
     var pagesHtml = '';
     if (this.htmlMode) {
       for (let page of pages) {
-        pagesHtml += '<div id="gd-page-' + page.number + '" class="gd-page" style="min-width: ' +
-          page.width + 'px; min-height: ' + page.height + 'px;">' +
+        pagesHtml += '<div id="gd-page-' + page.number + '" class="gd-page">' +
           '<div class="gd-wrapper">' + page.data + '</div>' +
           '</div>';
       }
     } else {
       for (let page of pages) {
-        pagesHtml += '<div id="gd-page-' + page.number + '" class="gd-page" style="min-width: ' +
-          page.width + 'px; min-height: ' + page.height + 'px;">' +
+        pagesHtml += '<div id="gd-page-' + page.number + '" class="gd-page">' +
           '<div class="gd-wrapper"><image style="width: inherit !important" class="gd-page-image" src="data:image/png;base64,' + page.data + '" alt></image></div>' +
           '</div>';
       }
     }
-    this.openWindow(pagesHtml);
+    this.openWindow(pagesHtml, pages[0].width, pages[0].height);
   }
 
-  private openWindow(pagesHtml) {
+  private openWindow(pagesHtml, width, height) {
+    const a4Height = 842;
+    const a4Width = 595;
+    var imageA4Adjusted = '';
+    if(width > a4Width && height > a4Height){
+      imageA4Adjusted = '.gd-page img { width: 21cm; height: 29.7cm;}';
+    }
     var cssPrint = '<style>' +
-      '.gd-page {height: 100% !important; page-break-after:always; page-break-inside: avoid; } .gd-page:last-child {page-break-after: auto;}';
+      '.gd-page { width: 21cm; height: 29.7cm; display: block; margin: auto; page-break-after:always; page-break-inside: avoid; }' +
+      ' .gd-page:last-child {page-break-after: auto;}' + imageA4Adjusted;
     cssPrint = cssPrint + '</style>';
 
     var windowObject = window.open('', "PrintWindow", "width=750,height=650,top=50,left=50,toolbars=yes,scrollbars=yes,status=yes,resizable=yes");
     windowObject.focus();
     windowObject.document.writeln(cssPrint);
     windowObject.document.writeln(pagesHtml);
-    windowObject.document.close();
+    //windowObject.document.close();
     windowObject.focus();
     windowObject.print();
-    windowObject.close();
+    //windowObject.close();
   }
 
   private renderPrintBlob(file: Blob) {
