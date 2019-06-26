cf1cf232723749ad8775af53c2c5368db0240c09
Pavel Egorov <egorovpasha@gmail.com>
2019-06-26 11:24:10 +0400

    exporting all components

--- libs/common-components/dist/groupdocs.examples.angular-common-components.d.ts
+++ libs/common-components/dist/groupdocs.examples.angular-common-components.d.ts
@@ -2,26 +2,3 @@
  * Generated bundle index. Do not edit.
  */
 export * from './index';
-export { BrowseFilesModalComponent as ɵf } from './lib/browse-files-modal/browse-files-modal.component';
-export { ButtonComponent as ɵb } from './lib/button/button.component';
-export { ChoiceButtonComponent as ɵi } from './lib/choice-button/choice-button.component';
-export { DisabledCursorDirective as ɵo } from './lib/disabled-cursor.directive';
-export { DndDirective as ɵk } from './lib/dnd.directive';
-export { DocumentComponent as ɵg } from './lib/document/document.component';
-export { ErrorModalComponent as ɵs } from './lib/error-modal/error-modal.component';
-export { InitStateComponent as ɵq } from './lib/init-state/init-state.component';
-export { LogoComponent as ɵc } from './lib/logo/logo.component';
-export { ModalComponent as ɵe } from './lib/modal/modal.component';
-export { PageComponent as ɵh } from './lib/page/page.component';
-export { PasswordRequiredComponent as ɵt } from './lib/password-required/password-required.component';
-export { RenderPrintDirective as ɵr } from './lib/render-print.directive';
-export { RotationDirective as ɵp } from './lib/rotation.directive';
-export { ScrollableDirective as ɵl } from './lib/scrollable.directive';
-export { SearchService as ɵv } from './lib/search.service';
-export { SearchComponent as ɵu } from './lib/search/search.component';
-export { SearchableDirective as ɵw } from './lib/searchable.directive';
-export { SelectComponent as ɵn } from './lib/select/select.component';
-export { TooltipComponent as ɵd } from './lib/tooltip/tooltip.component';
-export { TopToolbarComponent as ɵa } from './lib/top-toolbar/top-toolbar.component';
-export { UploadFileZoneComponent as ɵj } from './lib/upload-file-zone/upload-file-zone.component';
-export { ZoomDirective as ɵm } from './lib/zoom.directive';
