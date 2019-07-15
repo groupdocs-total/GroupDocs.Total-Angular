a02ab84aa2c27b86cf8e4a0a351b59d91dadfda2
Pavel Egorov <egorovpasha@gmail.com>
2019-07-15 09:52:57 +0400

    fixed DndDirective eventemitter names as suggested by linter

--- libs/common-components/src/lib/dnd.directive.ts
+++ libs/common-components/src/lib/dnd.directive.ts
@@ -6,8 +6,8 @@ import {UploadFilesService} from "./upload-files.service";
 })
 export class DndDirective {
 
-  @Output() close = new EventEmitter<boolean>();
-  @Output() open = new EventEmitter<boolean>();
+  @Output() closing = new EventEmitter<boolean>();
+  @Output() opening = new EventEmitter<boolean>();
   @Input() isBackground = true;
 
   @HostBinding('style.background') background = 'transparent';
@@ -22,7 +22,7 @@ export class DndDirective {
     if (this.isBackground) {
       this.background = '#999';
     } else {
-      this.open.emit(true);
+      this.opening.emit(true);
     }
   }
 
@@ -55,7 +55,7 @@ export class DndDirective {
   }
 
   private closeArea() {
-    this.close.emit(true);
-    this.open.emit(false);
+    this.closing.emit(true);
+    this.opening.emit(false);
   }
 }
