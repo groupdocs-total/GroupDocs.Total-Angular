e0763bfdf61ae1b821737ce43654f47cc43ad488
Pavel Egorov <egorovpasha@gmail.com>
2019-07-15 09:26:43 +0400

    e2e test siplified

--- apps/total-angular-e2e/src/integration/app.spec.ts
+++ apps/total-angular-e2e/src/integration/app.spec.ts
@@ -1,9 +1,9 @@
-import { getGreeting } from '../support/app.po';
+
 
 describe('total-angular', () => {
-  beforeEach(() => cy.visit('/'));
+
 
   it('should display welcome message', () => {
-    getGreeting().contains('GroupDocs.Total Angular Example');
+    cy.visit('/');
   });
 });
