e25e2ecb024c342130651bf26705239fc299320e
Egor <egor.krushinov@gmail.com>
2019-08-06 11:32:18 +0300

    WIP: correcting the top offset for the highlights on the second and further pages.

--- libs/comparison/src/lib/difference-highlight/difference-highlight.component.ts
+++ libs/comparison/src/lib/difference-highlight/difference-highlight.component.ts
@@ -22,8 +22,11 @@ export class DifferenceHighlightComponent implements OnInit{
     const y = this.change.box.y;
 
     // TODO: take this widths&heights using jQuery
-    const xOffcet = (1569 - 794)/2;
-    const yOffcet = 20;
+    const xOffcet = (1569 - 794)/2; // .panzoom width - .gd-page-image width
+    const yOffcet = this.change.pageInfo.id === 0 ? 
+                    20 : // .page margin
+                    // borders + .page margin + .page margins + page number * .gd-page-image height
+                    5 + 20 + 40 + (this.change.pageInfo.id * 1123);
 
     this.left = xOffcet + x;
     this.top = yOffcet + y;
