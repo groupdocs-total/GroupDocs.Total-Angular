d575a6d3554503bc63b7f23b8883e7417fa82466
Elena Kolesnikova <koleslena@yandex.ru>
2019-09-23 21:24:59 +0600

    fixed upload signatures panel

--- libs/signature/src/lib/upload-signature/upload-signature.component.ts
+++ libs/signature/src/lib/upload-signature/upload-signature.component.ts
@@ -11,42 +11,25 @@ export class UploadSignatureComponent implements OnInit {
   @Input() signatureType: string;
   @Input() rewrite: boolean;
   @Output() closePanel = new EventEmitter<boolean>();
-  files: FileList;
-  names = '';
-  addActive = false;
 
   constructor(private _signatureService: SignatureService) {
   }
 
   ngOnInit() {
-    this.clean();
-  }
-
-  private clean() {
-    this.files = null;
-    this.names = '';
-    this.addActive = false;
   }
 
   handleFileInput(files: FileList) {
-    this.setFiles(files);
+    this.addFiles(files);
   }
 
-  addFiles() {
-    const data = new FileListWithParams(this.files, this.signatureType);
+  addFiles(files) {
+    const data = new FileListWithParams(files, this.signatureType);
     this._signatureService.uploadSignature(data, this.rewrite).subscribe(() => {
-      this.clean();
       this.closePanel.emit(true);
     });
   }
 
-  setFiles(files: any) {
-    this.files = files;
-    for (let i = 0; i < files.length; i++) {
-      const file = files.item(i);
-      this.names += file.name + '\n';
-    }
-    this.names = this.names ? this.names.substring(0, this.names.length - 1) : this.names;
-    this.addActive = true;
+  uploadFiles($event: any) {
+    this.addFiles($event);
   }
 }
