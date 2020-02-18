956656cbb8affec95b2655d33975dc00b715e210
Egor <egor.krushinov@aspose.com>
2020-02-18 17:33:16 +0300

    Fixing CI build.

--- apps/total-angular-e2e/src/integration/metadata.spec.ts
+++ apps/total-angular-e2e/src/integration/metadata.spec.ts
@@ -8,6 +8,7 @@ describe('Metadata', () => {
     cy.fixture("conversionLoadConfigDefault").as('conversionLoadConfigDefault');
     cy.fixture("editorLoadConfigDefault").as('editorLoadConfigDefault');
     cy.fixture("signatureLoadConfigDefault").as('signatureLoadConfigDefault');
+    cy.fixture("annotationLoadConfigDefault").as('annotationLoadConfigDefault');
     cy.fixture("metadataLoadConfigDefault").as('metadataLoadConfigDefault');
     cy.fixture("loadFileTreeDefault").as('loadFileTreeDefault');
     cy.fixture("loadFileTreeSubFolder").as('loadFileTreeSubFolder');
@@ -20,6 +21,7 @@ describe('Metadata', () => {
     cy.route('http://localhost:8080/conversion/loadConfig', "@conversionLoadConfigDefault");
     cy.route('http://localhost:8080/editor/loadConfig', "@editorLoadConfigDefault");
     cy.route('http://localhost:8080/signature/loadConfig', "@signatureLoadConfigDefault");
+    cy.route('http://localhost:8080/annotation/loadConfig', "@annotationLoadConfigDefault");
     cy.route('http://localhost:8080/metadata/loadConfig', "@metadataLoadConfigDefault");
 
     cy.route('POST','http://localhost:8080/metadata/loadFileTree', "@loadFileTreeDefault");
