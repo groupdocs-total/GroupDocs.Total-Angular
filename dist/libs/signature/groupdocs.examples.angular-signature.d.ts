95ef548a11681e5dab4061f411abc9aed19b5be7
Travis CI <travis@travis-ci.org>
2020-02-21 10:11:03 +0000

    Build new version

--- dist/libs/signature/groupdocs.examples.angular-signature.d.ts
+++ dist/libs/signature/groupdocs.examples.angular-signature.d.ts
@@ -2,14 +2,13 @@
  * Generated bundle index. Do not edit.
  */
 export * from './index';
-export { CanvasComponent as ɵf } from './lib/canvas/canvas.component';
-export { DndSignatureDirective as ɵd } from './lib/dnd-signature.directive';
-export { HandModalComponent as ɵj } from './lib/hand-modal/hand-modal.component';
-export { NewBarQrCodeComponent as ɵb } from './lib/new-bar-qr-code/new-bar-qr-code.component';
-export { RemoveCanvasService as ɵh } from './lib/remove-canvas.service';
-export { SignatureLeftPanelComponent as ɵi } from './lib/signature-left-panel/signature-left-panel.component';
-export { SignatureTabComponent as ɵa } from './lib/signature-tab/signature-tab.component';
-export { Signature as ɵe } from './lib/signature/signature.component';
-export { StampCanvasComponent as ɵg } from './lib/stamp-canvas/stamp-canvas.component';
-export { StampModalComponent as ɵk } from './lib/stamp-modal/stamp-modal.component';
-export { UploadSignatureComponent as ɵc } from './lib/upload-signature/upload-signature.component';
+export { CanvasComponent as ɵe } from './lib/canvas/canvas.component';
+export { DndSignatureDirective as ɵc } from './lib/dnd-signature.directive';
+export { HandModalComponent as ɵi } from './lib/hand-modal/hand-modal.component';
+export { NewBarQrCodeComponent as ɵa } from './lib/new-bar-qr-code/new-bar-qr-code.component';
+export { RemoveCanvasService as ɵg } from './lib/remove-canvas.service';
+export { SignatureLeftPanelComponent as ɵh } from './lib/signature-left-panel/signature-left-panel.component';
+export { Signature as ɵd } from './lib/signature/signature.component';
+export { StampCanvasComponent as ɵf } from './lib/stamp-canvas/stamp-canvas.component';
+export { StampModalComponent as ɵj } from './lib/stamp-modal/stamp-modal.component';
+export { UploadSignatureComponent as ɵb } from './lib/upload-signature/upload-signature.component';
