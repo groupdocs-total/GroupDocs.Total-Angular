b28559489f14f5638012e8006c8949b20cda98aa
Vladimir Litvinchik <35294201+vladimir-litvinchik@users.noreply.github.com>
2020-12-02 14:36:57 +0200

    Fixed issue 'Cannot read property 'replace' of undefined' (#229)

--- libs/viewer/src/lib/excel-page/excel-page.component.ts
+++ libs/viewer/src/lib/excel-page/excel-page.component.ts
@@ -28,20 +28,22 @@ export class ExcelPageComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    // TODO: this is temporary needed to remove unneeded spaces and BOM symbol 
-    // which leads to undesired spaces on the top of the docs pages
-    this.data = this.data !== null ? this.data.replace(/>\s+</g,'><')
-                                              .replace(/\uFEFF/g,"")
-                                              .replace(/href="\/viewer/g, 'href="http://localhost:8080/viewer')
-                                              .replace(/src="\/viewer/g, 'src="http://localhost:8080/viewer')
-                                              .replace(/data="\/viewer/g, 'data="http://localhost:8080/viewer')
-                                   : null;
-    const dataImagePngBase64 = 'data:image/png;base64,';
-    this.imgData = dataImagePngBase64;
-    if (!this.isHtml) {
-      this.imgData += this.data;
+    if(this.isHtml) {
+      // TODO: this is temporary needed to remove unneeded spaces and BOM symbol 
+      // which leads to undesired spaces on the top of the docs pages
+      this.data = this.data 
+        ? this.data.replace(/>\s+</g,'><')
+        .replace(/\uFEFF/g,"")
+        .replace(/href="\/viewer/g, 'href="http://localhost:8080/viewer')
+        .replace(/src="\/viewer/g, 'src="http://localhost:8080/viewer')
+        .replace(/data="\/viewer/g, 'data="http://localhost:8080/viewer')
+      : null;
+    } else {
+      this.imgData = 'data:image/png;base64,' + this.data;
     }
 
-    this.data = this.data !== null ? this._excelPageService.getUpdatedPage(this.data) : null;
+    this.data = this.data !== null 
+      ? this._excelPageService.getUpdatedPage(this.data) 
+      : null;
   }
 }
