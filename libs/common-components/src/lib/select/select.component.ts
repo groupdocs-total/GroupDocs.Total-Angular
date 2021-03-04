47542f3689bdf650b1732072614b175dd075987b
Egor <egor.krushinov@aspose.com>
2021-03-04 14:57:27 +0300

    Add select closing on opening another in editor.

--- libs/common-components/src/lib/select/select.component.ts
+++ libs/common-components/src/lib/select/select.component.ts
@@ -18,6 +18,7 @@ export class SelectComponent {
   @Input() disabled = false;
   @Input() showSelected: Option;
   @Output() selected: EventEmitter<any> = new EventEmitter();
+  @Output() opened: EventEmitter<boolean> = new EventEmitter();
   @Input() isOpen = false;
   @Input() icon: string;
 
@@ -47,6 +48,7 @@ export class SelectComponent {
     $event.stopPropagation();
     if (!this.disabled) {
       this.isOpen = !this.isOpen;
+      if (this.isOpen) this.opened.emit(true);
     }
   }
 
