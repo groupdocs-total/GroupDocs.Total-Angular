8ac92070585d686716389ee79d172719e0716f3f
Ekaterina Chernova <e.chernova@arammeem.com>
2023-08-28 11:14:08 +0300

    GROUPDOCSAPP-201: fix viewer scroll issue (load other pages when end reached)

--- libs/common-components/src/lib/scrollable-edited.directive.ts
+++ libs/common-components/src/lib/scrollable-edited.directive.ts
@@ -102,6 +102,13 @@ export class ScrollableEditedDirective implements AfterViewInit, OnChanges {
       // if we scrolled till the end, the current page number must be the last page
       if (this._elementRef.nativeElement.scrollTop + this._elementRef.nativeElement.offsetHeight + 30 >= this._elementRef.nativeElement.scrollHeight) {
         pageNum = pages.length;
+        counter = pageNum;
+        // load not loaded pages
+        while (!this.loadedPagesSet.has(counter)) {
+          this._pagePreloadService.changeLastPageInView(counter);
+          this.loadedPagesSet.add(counter);
+          counter--;
+        }
         break;
       }
 
